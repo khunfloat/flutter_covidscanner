66892f2a4d0d56257746b9070cbecce19df3e87d
Chayoot Kosiwanich <60844731+khunfloat@users.noreply.github.com>
2021-05-16 22:55:58 +0700

    use column widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,29 @@ class Myapp extends StatelessWidget {
           title: Text("covid scanner"),
         ),
         body: Center(
-          child: Image(
-            image: NetworkImage(
-                "https://static.wixstatic.com/media/dbcf41_9d84f4b38dae41e6a92dbd8330998b43~mv2.png/v1/fit/w_800%2Ch_800%2Cal_c/file.png"),
-          ),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: NetworkImage(
+                  "https://static.wixstatic.com/media/dbcf41_9d84f4b38dae41e6a92dbd8330998b43~mv2.png/v1/fit/w_800%2Ch_800%2Cal_c/file.png"),
+            ),
+            Text(
+              "What is the cat?",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.purple,
+              ),
+            ),
+            Text(
+              "I dont know dude.",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.cyan,
+              ),
+            ),
+          ],
+        )),
       ),
       theme: ThemeData(primarySwatch: Colors.green),
     );
