0cfd6171ec68257ecce139110a00ec2c17824e4c
Chayoot Kosiwanich <60844731+khunfloat@users.noreply.github.com>
2021-05-16 22:26:05 +0700

    create scaffold widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('datas'),
-        ),
-        body: Center(
-          child: Text('hello'),
-        ),
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("covid scanner"),
       ),
-    );
-  }
+      body: Text("hello"),
+    ),
+  );
+  runApp(app);
 }
